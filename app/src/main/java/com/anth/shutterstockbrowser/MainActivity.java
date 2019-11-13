01bbc62c4bb25e0b0fe55ed1cb31091a50ff32dd
Alexander Thompson <anthompson717@gmail.com>
2019-11-13 12:54:33 -0700

    start

---
+++ app/src/main/java/com/anth/shutterstockbrowser/MainActivity.java
@@ -0,0 +1,13 @@
+package com.anth.shutterstockbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
